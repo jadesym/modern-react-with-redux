0ac99dc6c729bba1cc881813dc6a5a155ef0633a
Kevin Fu <kevin.fu808@gmail.com>
2019-04-06 19:39:33 -0700

    Completed up to section 3

---
+++ install_node.sh
@@ -0,0 +1,7 @@
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
+
+source ~/.bashrc
+
+nvm install <INSERT_VERSION_NUMBER_HERE>
+nvm use <INSERT_VERSION_NUMBER_HERE>
